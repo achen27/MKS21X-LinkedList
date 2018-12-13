5e74de791287efb3c1f74ebd70bb7ed3840f79c4
Amanda Chen <achen01@stuy.edu>
2018-12-13 10:48:08 -0500

    fixed types in Node class and renamed methods

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,6 +1,7 @@
 class MyLinkedList{
+
   class Node{
-    private int data;
+    private Interger data;
     private Node next,prev;
 
     public Node(int d, Node n, Node p){
@@ -12,46 +13,52 @@ class MyLinkedList{
     public int getData(){
       return data;
     }
-
-    public Node getNext(){
+    public Node next(){
       return next;
     }
-
-    public Node getPrev(){
+    public Node prev(){
       return prev;
     }
 
-    public void setData(int d){
-      data = d;
+    public void setData(Integer i){
+      data = i;
     }
-
     public void setNext(Node n){
       next = n;
     }
-
     public void setPrev(Node p){
       prev = p;
     }
+
+    public String toString(){
+    return "";
+    }
   }
+
   private int size;
   private Node start,end;
+
+  //public MyLinkedList();
 
   public int size(){
     return size;
   }
+
   public boolean add(int value){
     Node newEnd = new Node(value, end, null);
     end.setNext(newEnd);
     return true;
   }
+
   public String toString(){
     String output = "";
     Node current = start;
-    while(current.getNext() != null){
+    while(current.next() != null){
       output += current.getData() + ", ";
-      current = current.getNext();
+      current = current.next();
     }
     output += end.getData();
     return output;
   }
+
 }
